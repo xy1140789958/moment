2d358920d68fa496e55c756b7b7ce4d0f77bb02b
xiangyu <1140789958@qq.com>
2022-01-22 15:30:22 +0800

    测试提交

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,6 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
-
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login'] // no redirect whitelist
